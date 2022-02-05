7d0bef1c9eba0b8b1191d4dff0644d9f28d98a77
Andy Lin <lin383@wisc.edu>
2022-02-05 14:55:02 -0600

    Moved some variables to .env

--- scripts/MyScript.js
+++ scripts/MyScript.js
@@ -7,10 +7,12 @@ async function main() {
   // console.log('Deploying contracts with the account:', deployer.address);
 
   // We get the contract to deploy
-  const FCat = await hre.ethers.getContractFactory('FloatingCats');
+  const FCat = await hre.ethers.getContractFactory(
+    process.env.VITE_CONTRACT_NAME
+  );
   const fcat = await FCat.deploy(
-    'FloatingCats', // name
-    'FCAT', // symbol
+    process.env.VITE_CONTRACT_NAME, // name
+    process.env.VITE_CONTRACT_SYMBOL, // symbol
     // 'ipfs://QmSZyYG4JQDd5M5H3e4ZtFh1GGqptR2Yyqo7SLrnYri3Tm/' // cid, remember the '/' at the end
     `ipfs://${process.env.FC_TEST_CID}/` // cid, remember the '/' at the end
   );
--- test/MyTest.js
+++ test/MyTest.js
@@ -12,59 +12,62 @@ require('dotenv').config();
 // const recipient = '0x6A8fb854C5CDA974626f5091d38Eb66213600FfB';
 // const metadataURI = 'cid/test.png';
 
+let tokenInstance;
+let hardhatTokenInstance;
+let owner;
+let buyer;
+let tokenPrice = process.env.VITE_ETHER_COST * 1e18; // in wei
+let tokenMaxSupply = process.env.VITE_ETHER_MAX_SUPPLY;
+let numberOfTokens;
+
 describe('MyNFT', function () {
+  /**
+   * # Test 01
+   * On initial contract deployment, total supply amount should
+   * be the same as owner's balance. This means balanceOf() should
+   * return the entire supply amount.
+   *
+   */
   it('Deployment should assign the total supply of tokens to the owner', async function () {
-    const [owner] = await ethers.getSigners();
-    const FCat = await ethers.getContractFactory('FloatingCats'); // token
+    const [account0, account1] = await ethers.getSigners();
+    const FCat = await ethers.getContractFactory(
+      process.env.VITE_CONTRACT_NAME
+    ); // token
     const fcat = await FCat.deploy(
-      'FloatingCats',
-      'FCat',
+      process.env.VITE_CONTRACT_NAME,
+      process.env.VITE_CONTRACT_SYMBOL,
       `ipfs://${process.env.FC_TEST_CID}/`
     ); // hardhat token
 
-    const ownerBalance = await fcat.balanceOf(owner.address);
+    console.log('account0: ', account0.address);
+    console.log('account1: ', account1.address);
+
+    // initialize test variables
+    tokenInstance = FCat;
+    hardhatTokenInstance = fcat;
+    owner = account0.address;
+    buyer = account1.address;
+
+    // deplyment greeting message
+    console.log('Your address: ', buyer);
+    console.log('Running Test 1, Deployment:');
+
+    // ownerBalance at contract init deployment should equal totalSupply
+    const ownerBalance = await fcat.balanceOf(owner);
     expect(await fcat.totalSupply()).to.equal(ownerBalance);
+  });
 
-    // return fcat
-    //   .deployed()
-    //   .then(function (instance) {
-    //     tokenSaleInstance = instance;
-    //     return tokenSaleInstance.address;
-    //   })
-    //   .then(function (address) {
-    //     assert.notEqual(address, 0x0, 'has contract address');
-    //     return tokenSaleInstance.tokenContract();
-    //   })
-    //   .then(function (address) {
-    //     assert.notEqual(address, 0x0, 'has token contract address');
-    //     return tokenSaleInstance.tokenPrice();
-    //   })
-    //   .then(function (price) {
-    //     assert.equal(price, tokenPrice, 'token price is correct');
-    //   });
+  /**
+   *
+   */
+  it('Mint and transfer an NFT to buyer', async function () {
+    // buyer at contract init deplyment should have 0 token
+    let balance = await hardhatTokenInstance.balanceOf(buyer);
+    expect(balance).to.equal(0);
 
-    // let balance = await fcat.balanceOf(recipient);
-    // expect(balance).to.equal(0);
-
-    // // uncomment this test and expect to see error msg: 'Need to pay up!'
-    // // const newlyMintedToken = await fcat.payToMint(recipient, metadataURI, {
-    // //   value: ethers.utils.parseEther('0.01'),
-    // // });
-
-    // const newlyMintedToken = await fcat.payToMint(recipient, metadataURI, {
-    //   value: ethers.utils.parseEther('0.05'),
-    // });
-
-    // // wait until the transaction is mined
-    // await newlyMintedToken.wait();
-
-    // balance = await fcat.balanceOf(recipient);
-    // expect(balance).to.equal(1);
-
-    // expect(await fcat.isContentOwned(metadataURI)).to.equal(true);
-    // const newlyMintedToken2 = await fcat.payToMint(recipient, 'foo', {
-    //   value: ethers.utils.parseEther('0.05'),
-    // });
+    const newlyMintedToken = await hardhatTokenInstance.mint(1, {
+      value: ethers.utils.parseEther('0.01'),
+    });
   });
   // TODO: add more tests
   // var tokenInstance;
