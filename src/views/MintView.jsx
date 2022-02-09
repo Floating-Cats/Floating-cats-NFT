82e1cbb063b827e0cb3afb6e650fd04ea0cc0932
Andy Lin <lin383@wisc.edu>
2022-02-08 19:37:00 -0600

    Added a form to Mint view

--- src/views/MintView.jsx
+++ src/views/MintView.jsx
@@ -1,5 +1,12 @@
-import Mint from '../components/Mint';
 import { useEffect, useState } from 'react';
+
+// component imports
+// import Mint from '../components/Mint';
+
+// bootstrap imports
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 // other imports
 const env = import.meta.env;
@@ -9,8 +16,15 @@ import { toast } from 'react-toastify';
 const MintView = ({ contract }) => {
   const [mintAmount, setMintAmount] = useState(1);
 
-  // TODO: get mint amount from user
-  const getMintAmount = () => {
+  // Clear the form on modal close.
+  useEffect(() => {
+    clearForm();
+  }, []);
+
+  /**
+   * Initializes the states (mintAmounts) used for the form.
+   */
+  const clearForm = () => {
     setMintAmount(1);
   };
 
@@ -22,11 +36,12 @@ const MintView = ({ contract }) => {
       console.log('1:  ', result);
     });
 
-    console.log('3:  ', env.VITE_COST);
+    const cost = env.VITE_COST * mintAmount;
+    console.log('cost = ', cost);
 
     const result = await contract
       .mint(mintAmount, {
-        value: ethers.utils.parseEther(env.VITE_COST),
+        value: ethers.utils.parseEther(cost.toString()),
       })
       // TODO: Changed this to a pending toaster
       .then(() => {
@@ -44,38 +59,60 @@ const MintView = ({ contract }) => {
   };
 
   return (
-    <div className='mintPageBg'>
-      <img src='pics/mint-bg-top.png' alt='' id='mint-bg' />
-      <div className='container' id='mintPage'>
-        <div className='row'>
-          <div className='col'>
-            <img
-              id='mintBtn'
-              src='pics/mint-btn-blue.png'
-              alt=''
-              onClick={() => mintToken()}
-            />
-          </div>
-          <div className='col'>
-            <img
-              id='mintBtn'
-              src='pics/mint-btn-red.png'
-              alt=''
-              onClick={() => mintToken()}
-            />
-          </div>
-          <div className='col'>
-            <img
-              id='mintBtn'
-              src='pics/mint-btn-yellow.png'
-              alt=''
-              onClick={() => mintToken()}
-            />
+    <>
+      <div>
+        <Row>
+          <Col xs={1}>
+            <Form>
+              <Form.Group>
+                <Form.Label>Quantity</Form.Label>
+                <Form.Control
+                  required
+                  id='item-quantity'
+                  type='number'
+                  placeholder='a number'
+                  value={mintAmount}
+                  onChange={(e) => setMintAmount(e.target.value)}
+                />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col xs={9}></Col>
+        </Row>
+      </div>
+      <div className='mintPageBg'>
+        <img src='pics/mint-bg-top.png' alt='' id='mint-bg' />
+        <div className='container' id='mintPage'>
+          <div className='row'>
+            <div className='col'>
+              <img
+                id='mintBtn-blue'
+                src='pics/mint-btn-blue.png'
+                alt=''
+                onClick={() => mintToken()}
+              />
+            </div>
+            <div className='col'>
+              <img
+                id='mintBtn-red'
+                src='pics/mint-btn-red.png'
+                alt=''
+                onClick={() => mintToken()}
+              />
+            </div>
+            <div className='col'>
+              <img
+                id='mintBtn-yellow'
+                src='pics/mint-btn-yellow.png'
+                alt=''
+                onClick={() => mintToken()}
+              />
+            </div>
           </div>
         </div>
+        <img src='pics/mint-bg-bt.png' alt='' id='mint-bg' />
       </div>
-      <img src='pics/mint-bg-bt.png' alt='' id='mint-bg' />
-    </div>
+    </>
   );
 };
 
