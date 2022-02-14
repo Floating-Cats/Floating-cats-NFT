f4849d879bb15f1ff8a8019fca92b608fdf35582
Andy Lin <lin383@wisc.edu>
2022-02-13 23:53:39 -0600

    Changed the format of the connected user public address

--- components/Accounts.tsx
+++ components/Accounts.tsx
@@ -1,32 +1,34 @@
-import type { BigNumber } from '@ethersproject/bignumber'
-import { formatEther } from '@ethersproject/units'
-import type { Web3ReactHooks } from '@web3-react/core'
-import { useEffect, useState } from 'react'
+import type { BigNumber } from '@ethersproject/bignumber';
+import { formatEther } from '@ethersproject/units';
+import type { Web3ReactHooks } from '@web3-react/core';
+import { useEffect, useState } from 'react';
 
 function useBalances(
   provider?: ReturnType<Web3ReactHooks['useProvider']>,
   accounts?: string[]
 ): BigNumber[] | undefined {
-  const [balances, setBalances] = useState<BigNumber[] | undefined>()
+  const [balances, setBalances] = useState<BigNumber[] | undefined>();
 
   useEffect(() => {
     if (provider && accounts?.length) {
-      let stale = false
+      let stale = false;
 
-      void Promise.all(accounts.map((account) => provider.getBalance(account))).then((balances) => {
+      void Promise.all(
+        accounts.map((account) => provider.getBalance(account))
+      ).then((balances) => {
         if (!stale) {
-          setBalances(balances)
+          setBalances(balances);
         }
-      })
+      });
 
       return () => {
-        stale = true
-        setBalances(undefined)
-      }
+        stale = true;
+        setBalances(undefined);
+      };
     }
-  }, [provider, accounts])
+  }, [provider, accounts]);
 
-  return balances
+  return balances;
 }
 
 export function Accounts({
@@ -34,13 +36,13 @@ export function Accounts({
   provider,
   ENSNames,
 }: {
-  accounts: ReturnType<Web3ReactHooks['useAccounts']>
-  provider: ReturnType<Web3ReactHooks['useProvider']>
-  ENSNames: ReturnType<Web3ReactHooks['useENSNames']>
+  accounts: ReturnType<Web3ReactHooks['useAccounts']>;
+  provider: ReturnType<Web3ReactHooks['useProvider']>;
+  ENSNames: ReturnType<Web3ReactHooks['useENSNames']>;
 }) {
-  const balances = useBalances(provider, accounts)
+  const balances = useBalances(provider, accounts);
 
-  if (accounts === undefined) return null
+  if (accounts === undefined) return null;
 
   return (
     <div>
@@ -49,12 +51,27 @@ export function Accounts({
         {accounts.length === 0
           ? 'None'
           : accounts?.map((account, i) => (
-              <ul key={account} style={{ margin: 0, overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                {ENSNames?.[i] ?? account}
-                {balances?.[i] ? ` (Ξ${formatEther(balances[i])})` : null}
+              <ul
+                key={account}
+                style={{
+                  margin: 0,
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                <li>
+                  {ENSNames?.[i] ??
+                    `${account.substring(0, 6)}...${account.substring(
+                      account.length - 6,
+                      account.length
+                    )}`}
+                </li>
+                <li>
+                  {balances?.[i] ? ` (Ξ${formatEther(balances[i])})` : null}
+                </li>
               </ul>
             ))}
       </b>
     </div>
-  )
+  );
 }
