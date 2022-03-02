91c3bb86a7dc2220fac3b55208f88de1876c56c2
Chiwen.L <zxcvbnm840508@gmail.com>
2022-03-01 20:14:16 -0800

    flipcards fixed for safari

--- components/FCRoadmap/index.tsx
+++ components/FCRoadmap/index.tsx
@@ -9,12 +9,19 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner' id='FCI-B'>
                 <div className='flip-card-front' id='FC-B'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/shelter.PNG' alt='' width='300' height='200' />
+                  <p>Shelter Donations</p>
                 </div>
                 <div className='flip-card-back' id='FC-B'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>Q1 - 2022</h2>
+
+                  <h4>
+                    We will donate <strong>10%</strong> of our earnings to
+                    organizations or groups relevant to animal care. In addition
+                    to that, we plan to partner with these organizations or
+                    groups as we hope to provide their needs and share their
+                    burden, so that more cats can be taken care of. 🏩
+                  </h4>
                 </div>
               </div>
             </div>
@@ -23,12 +30,18 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner' id='FCI-B'>
                 <div className='flip-card-front' id='FC-B'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/sandbox.PNG' alt='' width='300' height='200' />
+                  <p>Sandbox Coummuity Hub</p>
                 </div>
                 <div className='flip-card-back' id='FC-B'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>Q3 - 2022</h2>
+                  <h4>
+                    We will build a community hub in the Sandbox for our
+                    community members to socialize and play mini-games! 👾
+                    <br /> <strong>Sandbox Avatar Development Commences</strong>
+                    <br /> We will make a series of Sandbox avatars. This will
+                    be airdrop for holders.📦
+                  </h4>
                 </div>
               </div>
             </div>
@@ -39,12 +52,20 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner'>
                 <div className='flip-card-front'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/vault.PNG' alt='' width='300' height='200' />
+                  <p>
+                    community vault & <br /> Giveaways
+                  </p>
                 </div>
                 <div className='flip-card-back'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>Q1 - 2022</h2>
+                  <h4>
+                    We will set up a community wallet and giveaway
+                    <strong> 5%</strong> of the initial sales to our holders.
+                    <br />
+                    <strong> 30%</strong> of the OS sales will go to the
+                    community wallet for giveawys and event funds. 💰
+                  </h4>
                 </div>
               </div>
             </div>
@@ -53,12 +74,19 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner'>
                 <div className='flip-card-front'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/merch.PNG' alt='' width='300' height='200' />
+                  <p>Exclusive Merchandises</p>
                 </div>
                 <div className='flip-card-back'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>Q2 - 2022</h2>
+                  <h4>
+                    We will release the first colletion of our floating brand.
+                    Anything from clothing to lifestyle items will be avalible
+                    for our holders.
+                    <br />
+                    <br />
+                    <strong>‼️ Weekly holders only giveaways 🎁</strong>
+                  </h4>
                 </div>
               </div>
             </div>
@@ -67,12 +95,19 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner'>
                 <div className='flip-card-front'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/Fcoin.PNG' alt='' width='300' height='200' />
+                  <p>
+                    $FLOAT Token & <br /> P2E development
+                  </p>
                 </div>
                 <div className='flip-card-back'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>Q4 - 2022</h2>
+                  <h4>
+                    <strong>$FLOAT Staking</strong> <br />
+                    The amount depends on the number of NFTs you hold +
+                    contributions you make in the server. $FLOAT can be used to
+                    purchase our merch and in-store items. 🪙
+                  </h4>
                 </div>
               </div>
             </div>
@@ -83,12 +118,22 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner' id='FCI-B'>
                 <div className='flip-card-front' id='FC-B'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/cafe.PNG' alt='' width='300' height='200' />
+                  <p>Floating Cat Cafe</p>
                 </div>
                 <div className='flip-card-back' id='FC-B'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>Q1 - 2023</h2>
+                  <h4>
+                    ☕ Opening Floating Cats Café has always been our dream. The
+                    cafe will partner and assist with local shelters, and
+                    provide space for those kitties with care and love.🧡
+                    Meanwhile, the space is to be served as an interactive way
+                    to encourage adoption.
+                    <br />
+                    <strong>
+                      Hoders will have free items/discounts for every visits. 🎉
+                    </strong>
+                  </h4>
                 </div>
               </div>
             </div>
@@ -97,12 +142,17 @@ export default function FCRoadmap() {
             <div className='flip-card'>
               <div className='flip-card-inner' id='FCI-B'>
                 <div className='flip-card-front' id='FC-B'>
-                  <img src='/cafe.png' alt='' width='300' height='200' />
+                  <img src='/rocket.PNG' alt='' width='300' height='200' />
+                  <p>More and Beyound</p>
                 </div>
                 <div className='flip-card-back' id='FC-B'>
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                  <h2>🚀 🔜</h2>
+                  <ul>
+                    <li>3D Avatar</li>
+                    <li>Mobile Game</li>
+                    <li>Real life Meetups</li>
+                    <li>Travel Givawys</li>
+                  </ul>
                 </div>
               </div>
             </div>
