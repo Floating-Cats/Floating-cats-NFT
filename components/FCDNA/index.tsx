1396eec89be197247a3908c178a79f26b830db00
Chiwen.L <zxcvbnm840508@gmail.com>
2022-03-05 16:31:41 -0800

    DNA added

---
+++ components/FCDNA/index.tsx
@@ -0,0 +1,54 @@
+export default function FCNDA() {
+  return (
+    <div className='container' id='DNA'>
+      <h1>DNA</h1>
+      <div className='DNA-bg'>
+        <div className='row'>
+          <div className='col'>
+            <img src='/1of1.png' alt='' />
+          </div>
+          <div className='col' id='DNAinfo'>
+            <h3>1 of 1</h3>
+            <p>QTY: 15</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <img src='/super-rare.png' alt='' />
+          </div>
+          <div className='col' id='DNAinfo'>
+            <h3>Super Rare</h3>
+            <p>QTY: 280</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <img src='/rare.png' alt='' />
+          </div>
+          <div className='col' id='DNAinfo'>
+            <h3>Rare</h3>
+            <p>QTY: 883</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <img src='/semi-rare.png' alt='' />
+          </div>
+          <div className='col' id='DNAinfo'>
+            <h3>Semi-Rare</h3>
+            <p>QTY: 1766</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <img src='/common.png' alt='' />
+          </div>
+          <div className='col' id='DNAinfo'>
+            <h3>Common</h3>
+            <p>QTY: 2944</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
